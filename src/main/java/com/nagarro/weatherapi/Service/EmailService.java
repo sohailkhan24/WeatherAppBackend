275b706e605c7ca6fa6324ed02a1582be550951d
sohail <48204572+sohailkhan24@users.noreply.github.com>
2021-05-21 18:48:18 +0530

    ....

--- src/main/java/com/nagarro/weatherapi/Service/EmailService.java
+++ src/main/java/com/nagarro/weatherapi/Service/EmailService.java
@@ -14,40 +14,38 @@ import com.nagarro.weatherapi.Values.Values;
 
 @Service
 public class EmailService {
-	
+
 	@Autowired
 	private WeatherService WeatherService;
-	
+
 	@Autowired
 	private JavaMailSender javaMailSender;
-	
-	@Scheduled(cron = "0 20 18 * * ?")
+
+	@Scheduled(cron = "0 49 18 * * ?")
 	// @Scheduled(cron = "[Seconds] [Minutes] [Hours] [Day of month] [Month] [Day of
 	// week] [Year]")
 	// Send Periodic Mails
 	public void sendPeriodicEmail() {
-		Map<String,String> userData=new LinkedHashMap<>();	     
-	     userData.put("sohail.khan@nagarro.com","Amritsar");
-	     userData.put("sohailkhan4764@gmail.com","Amritsar");		
-	     for(String email:userData.keySet()) {
-	    	 String city=userData.get(email);
-	    	 Weather weatherData = WeatherService.getWeatherData(city);
-	    	 sendEmail(email, Values.date, city, weatherData.getDescription());
-	    	 System.out.println(weatherData);
-	     }
+		Map<String, String> userData = new LinkedHashMap<>();
+		userData.put("sohail.khan@nagarro.com", "Amritsar");
+		userData.put("sohailkhan4764@gmail.com", "Amritsar");
+		for (String email : userData.keySet()) {
+			String city = userData.get(email);
+			Weather weatherData = WeatherService.getWeatherData(city);
+			sendEmail(email, Values.date, city, weatherData.getDescription());
+			System.out.println(weatherData);
+		}
 
 	}
 
 //	Mail Service
 	public void sendEmail(String email, String date, String city, String description) {
 		SimpleMailMessage message = new SimpleMailMessage();
-		message.setFrom("Weather Service");
 		message.setTo(email);
 		message.setSubject("Daily  Weather Report");
-		message.setText("You have registered for the daily Weather Report@10am IST." + "\r\n\n" + "Today's"
-				+ " (Date: " + date + " ) weather report=> " + "\r\n\n" + "City:   " + city + "\r\n"
-				+ "Weather Description:   " + description + "\r\n\n"
-				+ "Nature is Powerfull,Stay Strong.\r\nThank you.\r\n");
+		message.setText("You have registered for the daily Weather Report@10am IST." + "\r\n\n" + "Today's" + " (Date: "
+				+ date + " ) weather report=> " + "\r\n\n" + "City:   " + city + "\r\n" + "Weather Description:   "
+				+ description + "\r\n\n" + "Nature is Powerfull,Stay Strong.\r\nThank you.\r\n");
 		javaMailSender.send(message);
 //		+ "\r\n" + "Temperature:  " + temp + " \u00B0C"
 	}
